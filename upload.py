1a20e1f9b850dec8ffbe2d91ee9591fda7d658fc
SpaceNerden <spacenerden@gmail.com>
2022-10-18 22:39:10 +0800

    Add Scheduling support

--- upload.py
+++ upload.py
@@ -3,8 +3,11 @@ from youtube_uploader_selenium import YouTubeUploader
 from typing import Optional
 
 
-def main(video_path: str, metadata_path: Optional[str] = None, thumbnail_path: Optional[str] = None):
-    uploader = YouTubeUploader(video_path, metadata_path, thumbnail_path)
+def main(video_path: str,
+         metadata_path: Optional[str] = None,
+         thumbnail_path: Optional[str] = None,
+         profile_path: Optional[str] = None):
+    uploader = YouTubeUploader(video_path, metadata_path, thumbnail_path, profile_path)
     was_video_uploaded, video_id = uploader.upload()
     assert was_video_uploaded
 
@@ -18,5 +21,7 @@ if __name__ == "__main__":
                         "--thumbnail",
                         help='Path to the thumbnail image',)
     parser.add_argument("--meta", help='Path to the JSON file with metadata')
+    parser.add_argument("--profile", help='Path to the firefox profile')
     args = parser.parse_args()
-    main(args.video, args.meta, args.thumbnail)
+
+    main(args.video, args.meta, args.thumbnail, profile_path=args.profile)
